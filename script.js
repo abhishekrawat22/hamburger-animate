7785872068a2ad447de701c5ad450802c158fe81
Abhishek Rawat <abhishekrawat5743@gmail.com>
2023-03-18 16:03:19 +0530

    Added JavaScript to make button animate on click.

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuButton = document.getElementsByClassName('menu-btn')[0];
+
+menuButton.addEventListener('click', function() {
+//   Toggling 'animate' class to apply desired animation when button is clicked.
+  menuButton.classList.toggle('animate');
+});
